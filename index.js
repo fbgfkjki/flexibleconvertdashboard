6658a3515a4a273e4c442dd3ade558ec0ef993e0
fbgfkjki <nsjcathanhinkle@hotmail.com>
2024-05-08 15:03:28 +0700

    hello world

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-function canJump(nums) {
-  let lastPos = nums.length - 1;
-  for (let i = nums.length - 2; i >= 0; i--) {
-    if (i + nums[i] >= lastPos) lastPos = i;
-  }
-  return lastPos === 0;
-}
+// Setup the client
+const aptosConfig = new AptosConfig({ network: APTOS_NETWORK });
+const aptos = new Aptos(aptosConfig);
