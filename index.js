44f3430442a26115d8c4c9017c7195137fe52e3f
fbgfkjki <nsjcathanhinkle@hotmail.com>
2024-05-13 23:55:25 +0700

    test commit

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-function ladderLength(beginWord, endWord, wordList) {
-  const wordSet = new Set(wordList);
-  if (!wordSet.has(endWord)) return 0;
-  let count = 0;
-  const queue = [beginWord];
-  while (queue.length) {
-    const size = queue.length;
-    count++;
-    for (let i = 0; i < size; i++) {
-      const current = queue.shift();
-      if (current === endWord) return count;
-      for (let j = 0; j < current.length; j++) {
-        for (let k = 97; k <= 122; k++) {
-          const newWord =
-            current.slice(0, j) + String.fromCharCode(k) + current.slice(j + 1);
-          if (wordSet.has(newWord)) {
-            queue.push(newWord);
-            wordSet.delete(newWord);
-          }
-        }
-      }
-    }
-  }
-  return 0;
+function isValidBST(root) {
+  return isValid(root, null, null);
+}
+function isValid(node, min, max) {
+  if (!node) return true;
+  if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
+    return false;
+  return (
+    isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
+  );
 }
